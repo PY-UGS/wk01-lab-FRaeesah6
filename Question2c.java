bdab6a0357fdc344162e6a4bebcde20d2fc6cf98
FRaeesah6 <fraeesah6@gmail.com>
2022-01-18 07:22:09 +0800

    Added all tutorial answers

---
+++ Question2c.java
@@ -0,0 +1,10 @@
+public class Question2c {
+    public static void main(String [] args){
+        for(int x=102; x>=66; x=x-1){
+            if(x%2==1) {
+                System.out.print("value of x : " + x);
+                System.out.print("\n");
+            }
+        }
+    }
+}
